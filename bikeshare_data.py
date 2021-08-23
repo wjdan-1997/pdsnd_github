c2b36e5d2e9598fc53dd035a50bc71ea298e614a
Your Name <wjdanjo2@outlook.sa>
2021-08-23 21:32:18 +0300

    updating the script code

--- bikeshare_data.py
+++ bikeshare_data.py
@@ -189,6 +189,13 @@ def user_stats(df):
     print("\nThis took %s seconds." % (time.time() - start_time))
     print('-'*40)
 
+    while True:
+        data= input('\nDo you want to see some data.\n').lower()
+    if data== 'yes':
+        print(df.iloc[0:,5:])
+    else:
+        print('\Great Thanks!')
+        break
 
 
 
