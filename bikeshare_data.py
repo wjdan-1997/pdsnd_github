f45f7b52f705d8c4a48ce396f37d0d02dd30bf77
Your Name <wjdanjo2@outlook.sa>
2021-08-24 09:25:09 +0300

    refactor: add some commint line

--- bikeshare_data.py
+++ bikeshare_data.py
@@ -209,7 +209,7 @@ def main():
         station_stats(df)
         trip_duration_stats(df)
         user_stats(df)
-
+        print('Lol Great')
         restart = input('\nWould you like to restart? Enter yes or no.\n')
         if restart.lower() != 'yes':
             print('Thanks I hope you Enjoyed.')
